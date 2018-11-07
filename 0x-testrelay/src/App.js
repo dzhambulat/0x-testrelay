b5f77f8e17def1e13684b94cfa48378f8b60e6cd
LTailor <dzkhasayev@gmail.com>
2018-11-07 18:32:40 +0300

    add maker order

--- 0x-testrelay/src/App.js
+++ 0x-testrelay/src/App.js
@@ -1,6 +1,19 @@
 import React, { Component } from 'react';
 import {getProviderEngine, getContractAddress, ROPSTEN_CONFIGS} from './utils/provider_engine';
+import { ContractAddresses, getContractAddressesForNetworkOrThrow } from '@0x/contract-addresses';
+import { DECIMALS, NULL_ADDRESS, ZERO } from './utils/constants';
 import { Web3Wrapper } from '@0x/web3-wrapper';
+import Web3 from 'web3';
+import {
+  assetDataUtils,
+  BigNumber,
+  ContractWrappers,
+  generatePseudoRandomSalt,
+  Order,
+  orderHashUtils,
+  signatureUtils,
+} from '0x.js';
+
 import './App.css';
 
 class App extends Component {
@@ -12,17 +25,90 @@ class App extends Component {
     this.makeSellOrder = this.makeSellOrder.bind(this);
     this.makeBuyOrder = this.makeBuyOrder.bind(this);
 
-    if(window.web3) {
-      const pe = getProviderEngine();
-      let _0xcontract = getContractAddress(ROPSTEN_CONFIGS.networkId);
+    window.addEventListener('load', async () => {
+    // Modern dapp browsers...
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum);
+      try {
+          // Request account access if needed
+          await window.ethereum.enable();
+          if(window.web3) {
+            
+            const pe = getProviderEngine();
+            this.providerEngine = pe;
+            this._0xcontract = getContractAddress(ROPSTEN_CONFIGS.networkId);
+            const addresses = getContractAddressesForNetworkOrThrow(ROPSTEN_CONFIGS.networkId);
+            this.zrxToken = addresses.zrxToken;
+            this.etherToken = addresses.etherToken;
+            this.web3Wrapper = new Web3Wrapper(pe);
+     
+            this.contractWrappers = new ContractWrappers(pe, { networkId: ROPSTEN_CONFIGS.networkId });
+         }
+        } catch (error) {
+          // User denied account access...\
+          console.log(error);
+        }
     }
+    // Legacy dapp browsers...
+    else if (window.web3) {
+        window.web3 = new Web3(window.web3.currentProvider);
+        if(window.web3) {
+          const pe = getProviderEngine();
+          this.providerEngine = pe;
+          this._0xcontract = getContractAddress(ROPSTEN_CONFIGS.networkId);
+          const addresses = getContractAddressesForNetworkOrThrow(ROPSTEN_CONFIGS.networkId);
+          this.zrxToken = addresses.zrxToken;
+          this.etherToken = addresses.etherToken;
+          this.web3Wrapper = new Web3Wrapper(pe);
+   
+          this.contractWrappers = new ContractWrappers(pe, { networkId: ROPSTEN_CONFIGS.networkId });
+       }
+    }
+    // Non-dapp browsers...
+    else {
+        console.log('Non-Ethereum browser detected. You should consider trying MetaMask!');
+      }
+    });
 
   }
-  makeSellOrder() {
-    alert('fff');
+  async makeSellOrder() {
+    alert('dd');
+
+    const [leftMaker] = await this.web3Wrapper.getAvailableAddressesAsync();
+    alert(leftMaker);
+    const makerAssetAmount = Web3Wrapper.toBaseUnitAmount(new BigNumber(10), DECIMALS);
+    const takerAssetAmount = Web3Wrapper.toBaseUnitAmount(new BigNumber(0.4), DECIMALS);
+    const makerAssetData = assetDataUtils.encodeERC20AssetData(this.zrxToken);
+    const takerAssetData = assetDataUtils.encodeERC20AssetData(this.etherToken);
+
+    const leftMakerZRXApprovalTxHash = await this.contractWrappers.erc20Token.setUnlimitedProxyAllowanceAsync(
+      this.zrxToken,
+      leftMaker,
+    );
+
+    const leftOrder = {
+      exchangeAddress: this._0xcontract,
+      makerAddress: leftMaker,
+      takerAddress: NULL_ADDRESS,
+      senderAddress: NULL_ADDRESS,
+      feeRecipientAddress: NULL_ADDRESS,
+      expirationTimeSeconds: 400,
+      salt: generatePseudoRandomSalt(),
+      makerAssetAmount,
+      takerAssetAmount,
+      makerAssetData,
+      takerAssetData,
+      makerFee: ZERO,
+      takerFee: ZERO,
+    };
+
+    const leftOrderHashHex = orderHashUtils.getOrderHashHex(leftOrder);
+    const leftOrderSignature = await signatureUtils.ecSignHashAsync(this.providerEngine, leftOrderHashHex, leftMaker);
+    const leftSignedOrder = { ...leftOrder, signature: leftOrderSignature };
+    alert(JSON.stringify(leftSignedOrder));
   }
   makeBuyOrder() {
-    alert('buy');
+
   }
   handleChange(event) {
     this.setState({signedOrder: event.target.value});
